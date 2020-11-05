f4bbf416f136d4ee1be1b2eb7cf7128443de32cc
JosephAbbey <66320873+JosephAbbey@users.noreply.github.com>
2020-11-05 16:51:03 +0000

    Update progressbars.js

--- progressbars.js
+++ progressbars.js
@@ -1,1 +1,50 @@
-async function setup(){function drawBar(a,b,c,d,e,f,g){const Bar=(canvas)=>{canvas.setup=async function(){if(c=='circle'){canvas.createCanvas(25/32*b,25/32*b);b=3/4*b;canvas.translate(canvas.width/2,canvas.height/2);for(var i=0;i<g+1;i++){canvas.clear();canvas.noFill();canvas.stroke(0,20);canvas.strokeWeight(5);canvas.ellipse(0,0,b);canvas.strokeWeight(10);canvas.stroke(lerpColor(e,f,map(i,0,100,0,1)));canvas.angleMode(DEGREES);canvas.arc(0,0,b,b,-90,map(i,0,100,-90,270));canvas.textAlign(CENTER,CENTER);canvas.textSize(b/7);canvas.noStroke();canvas.fill(0);canvas.text(i+'%',0,0);await sleep(3)}}else if(c=='line'){canvas.createCanvas(25/32*b,12);b=3/4*b;canvas.translate(canvas.width/2,canvas.height/2);canvas.noFill();canvas.stroke(0,20);canvas.strokeWeight(5);canvas.line(0-b/2,0,0+b/2,0);canvas.strokeWeight(10);for(var j=0;j<g+1;j++){canvas.stroke(lerpColor(e,f,map(j,0,100,0,1)));canvas.line(0-b/2,0,map(j,0,100,0,b)-b/2,0);await sleep(3)}}}}new p5(Bar,a)}}function sleep(a){return new Promise(resolve=>setTimeout(resolve,a))}
+function drawBar(where, size, type, bg, c1, c2, endval) {
+  const Bar = (canvas) => {
+    canvas.setup = async function() {
+      if (type == 'circle') {
+        canvas.createCanvas(25 / 32 * size, 25 / 32 * size);
+        size = 3 / 4 * size;
+        canvas.translate(canvas.width / 2, canvas.height / 2);
+        for (var i = 0; i < endval + 1; i++) {
+          canvas.clear();
+          canvas.noFill();
+          canvas.stroke(0, 20);
+          canvas.strokeWeight(5);
+          canvas.ellipse(0, 0, size);
+          canvas.strokeWeight(10);
+          canvas.stroke(lerpColor(c1, c2, map(i, 0, 100, 0, 1)));
+          canvas.angleMode(DEGREES);
+          canvas.arc(0, 0, size, size, -90, map(i, 0, 100, -90, 270));
+          canvas.textAlign(CENTER, CENTER);
+          canvas.textSize(size / 7);
+          canvas.noStroke();
+          canvas.fill(0);
+          canvas.text(i + '%', 0, 0);
+          await sleep(3);
+        }
+      } else if (type == 'line') {
+        canvas.createCanvas(25 / 32 * size, 12);
+        size = 3 / 4 * size;
+        canvas.translate(canvas.width / 2, canvas.height / 2);
+        canvas.noFill();
+        canvas.stroke(0, 20);
+        canvas.strokeWeight(5);
+        canvas.line(0 - size / 2, 0, 0 + size / 2, 0);
+
+        canvas.strokeWeight(10);
+        for (var j = 0; j < endval + 1; j++) {
+          canvas.stroke(lerpColor(c1, c2, map(j, 0, 100, 0, 1)));
+          canvas.line(0 - size / 2, 0, map(j, 0, 100, 0, size) - size / 2, 0);
+          await sleep(3);
+        }
+      }
+    }
+  }
+  new p5(Bar, where);
+}
+
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
